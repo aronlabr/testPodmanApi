4337bbddc39cef7d83fa6ef94d48dbd5e5b9981c
Luis Aaron <71951748+aronlabr@users.noreply.github.com>
2024-09-08 16:19:54 -0500

    Update test code

--- src/extension.ts
+++ src/extension.ts
@@ -25,60 +25,37 @@ export function initTelemetryLogger(): void {
 // Activate the extension asynchronously
 export async function activate(extensionContext: extensionApi.ExtensionContext): Promise<void> {
   initTelemetryLogger();
-  await getDistros()
-
-  // const octokit = new Octokit();
-  // const detect = new Detect(extensionContext.storagePath);
-
-  // const composeGitHubReleases = new GitHubReleases(octokit, 'docker', 'compose');
-  // const composeDownload = new Download(extensionContext, composeGitHubReleases);
-
-  // const podmanGitHubReleases = new GitHubReleases(octokit, 'containers', 'podman');
-  // const podmanDownload = new Download(extensionContext, podmanGitHubReleases);
-
-  // // ONBOARDING: Command to check compose is downloaded
-  // const onboardingCheckDownloadCommand = extensionApi.commands.registerCommand(
-  //   'compose.onboarding.checkDownloadedCommand',
-  //   async () => {
-  //     const isDownloaded = await detect.getStoragePath();
-  //     if (isDownloaded === '') {
-  //       extensionApi.context.setValue('composeIsNotDownloaded', true, 'onboarding');
-  //     } else {
-  //       extensionApi.context.setValue('composeIsNotDownloaded', false, 'onboarding');
-  //     }
-
-  //     await handler.updateConfigAndContextComposeBinary(extensionContext);
-
-  //     if (!isDownloaded) {
-  //       // Get the latest version and store the metadata in a local variable
-  //       const composeLatestVersion = await composeDownload.getLatestVersionAsset();
-  //       // Set the value in the context to the version we're downloading so it appears in the onboarding sequence
-  //       if (composeLatestVersion) {
-  //         composeVersionMetadata = composeLatestVersion;
-  //         extensionApi.context.setValue('composeDownloadVersion', composeVersionMetadata.tag, 'onboarding');
-  //       }
-  //     }
-
-  //     telemetryLogger?.logUsage('compose.onboarding.checkDownloadedCommand', {
-  //       downloaded: isDownloaded === '' ? false : true,
-  //       version: composeVersionMetadata?.tag,
-  //     });
-  //   },
-  // );
-
-  // Push the commands that will be used within the onboarding sequence
-  // extensionContext.subscriptions.push(
-  //   onboardingCheckDownloadCommand,
-  // );
+  await getDistros();
 
   let testInput = await extensionApi.window.showInputBox({
     title: "Hello",
     markdownDescription: "Hola extension",
-    valueSelection: [1,2]
-  })
+    valueSelection: [1, 2]
+  });
   // extensionApi.commands.registerCommand(`${extInfo.id}.once`, testInput)
   // Create a provider with an example name, ID and icon
   const provider = extensionApi.provider.createProvider(extInfo);
+  const myFirstCommand = extensionApi.commands.registerCommand(`${extInfo.id}.onboarding.testCommand`, async () => {
+    // display a choice to the user for selecting some values
+    const result = await extensionApi.window.showQuickPick(['un', 'deux', 'trois'], {
+      canPickMany: true, // user can select more than one choice
+    });
+
+    // display an information message with the user choice
+    await extensionApi.window.showInformationMessage(`The choice was: ${result}`);
+  });
+
+  // create an item in the status bar to run our command
+  // it will stick on the left of the status bar
+  const item = extensionApi.window.createStatusBarItem(extensionApi.StatusBarAlignLeft, 100);
+  item.text = 'My first command';
+  item.command = `${extInfo.id}.command`;
+  item.show();
+
+  extensionContext.subscriptions.push(
+    myFirstCommand,
+    item
+  );
 
   // Push the new provider to Podman Desktop
   extensionContext.subscriptions.push(provider);
