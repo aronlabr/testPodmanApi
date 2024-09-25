f16dcc5f02b85a15f93e616e2870542423420b3b
Luis Aaron <71951748+aronlabr@users.noreply.github.com>
2024-09-25 00:38:41 -0500

    Update ext

--- src/wsl.ts
+++ src/wsl.ts
@@ -18,9 +18,9 @@ export async function getDistros(): Promise<WSLInstance[]> {
   // Process each line and create JSON object for each WSL distro
   const distros: WSLInstance[] = lines.map(line => {
     // Use regex to match the first column (name), second (state), and third (version)
-    const cols = line.match(/^\s*([^\s]+)\s+(\w+)\s+(\d+)\s*$/)
+    const cols = line.match(/^\s*([A-Za-z\s]+?)\s+([A-Za-z\s]+?)\s+(\d+)\s*$/)
     if (!cols) console.log(`Invalid line format: ${line}`)
-    console.log(`Cols lenght: ${cols?.length}`)
+    console.log(`Cols lenght: ${cols}`)
     const [_, name, state, version] = cols ? cols : ['','','',''];
     return { name, state, version: parseInt(version) };
     
